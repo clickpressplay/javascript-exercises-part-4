798b5eaeeaa6fef515fbc007d1d348b61d47eb7d
clickpressplay <121888480+clickpressplay@users.noreply.github.com>
2023-01-04 15:45:14 -0500

    Passed check - repeatString while loop

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,24 @@
-const repeatString = function() {
 
-};
+let repeatedString = repeatString('hey', 3);
+console.log(repeatedString);
+
+
+function repeatString(input, repeats){
+
+    let baseString = input;
+    let finalResult = input;
+    let i = 0;
+
+    while(i < repeats - 1){
+
+        finalResult += baseString;
+        i++;
+        
+
+    }
+
+    return finalResult;
+}
 
 // Do not edit below this line
 module.exports = repeatString;
