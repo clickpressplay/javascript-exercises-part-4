ca6b46c819586bb36366b8ad2624700f00d80c71
clickpressplay <121888480+clickpressplay@users.noreply.github.com>
2023-01-19 17:53:18 -0500

    All tests passed

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,20 @@
-const leapYears = function() {
+const leapYears = function(year) {
+
+// returns undefined without else clause when running npm test leapYears.spec.js
+
+if ((year % 4 == 0 && year % 100 !== 0) || (year % 4 == 0 && year % 400 == 0)){
+
+    return true;
+
+} else {
+
+    return false;
+
+}
+
+
+
+
 
 };
 
