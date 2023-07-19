3d269dd46bbd3f97a90a790decfdef66760c5b39
clickpressplay <mramocsai@gmail.com>
2023-07-18 20:24:24 -0400

    Always pay attention to value of base integer

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,51 @@
-const sumAll = function() {
+// initialize smallest value as new variable
+// initialize largest value as stopping point
+// iterate on smallest value until stopping point is reached 
+
+const sumAll = function(x, y) {
+    
+    
+    let sum;
+    let floor;
+    let ceiling;
+
+    if (!Number.isInteger(x) || !Number.isInteger(y)){
+
+        return "ERROR";
+
+    }
+
+    if ((x || y) < 0){
+
+        return "ERROR";
+
+    }
+
+
+    if (x > y){
+
+        floor = y;
+        ceiling = x;
+        sum = floor;
+
+    } else {
+
+        floor = x;
+        ceiling = y;
+        sum = floor;
+
+
+    }
+
+
+    for (let i = floor; i < ceiling; i++ ){
+
+        sum += (floor + 1); 
+        floor++;
+
+    }
+
+    return sum;
 
 };
 
