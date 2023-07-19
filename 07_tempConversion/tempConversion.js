a0380ccc261641ea87f06c43cd2ed46eaa022020
clickpressplay <121888480+clickpressplay@users.noreply.github.com>
2023-07-19 16:30:00 -0400

    Rounding value to one decimal place: Math.round(value * 10) / 10

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+// All tests passed
+// Rounding value to one decimal place: Math.round(value * 10) / 10
+
+const convertToCelsius = function(fahrenheit) {
+
+  let celsius = Math.round(((fahrenheit - 32) * (5/9)) * 10) / 10;
+  return celsius;
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+
+  let fahrenheit = Math.round((celsius * (9/5) + 32) * 10) / 10;
+  return fahrenheit;
+
 };
 
 // Do not edit below this line
